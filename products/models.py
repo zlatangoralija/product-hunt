e8449f68f91549481218ae0804b55fec9082ed62
zlatan-ubuntu <zlatan@ubuntu.com>
2020-08-10 20:25:35 +0200

    Product class and migration

--- products/models.py
+++ products/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    url = models.TextField()
+    pub_date = models.DateField()
+    votes_total = models.BigIntegerField(default=1)
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='images/')
+    body = models.TextField()
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def pub_date_pretty(self):
+        return  self.pub_date.strftime('%b %e %Y')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+
