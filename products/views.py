ccb26d62058f9e18316b1589e4d9ea4e49208240
zlatan-ubuntu <zlatan@ubuntu.com>
2020-08-11 20:42:22 +0200

    polish

--- products/views.py
+++ products/views.py
@@ -9,7 +9,7 @@ def home(request):
     products = Product.objects.all
     return render(request, 'products/home.html', {'products': products})
 
-@login_required
+@login_required(login_url="/accounts/login/")
 def create(request):
     if request.method == 'POST':
         if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['image'] and request.FILES['icon']:
@@ -33,12 +33,12 @@ def create(request):
     else:
         return render(request, 'products/create.html')
 
-@login_required
+@login_required(login_url="/accounts/login/")
 def detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     return render(request, 'products/detail.html', {'product': product})
 
-@login_required
+@login_required(login_url="/accounts/login/")
 def upvote(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     product.votes_total += 1
